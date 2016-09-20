59fd2466e3308b0cc8be7cbf7ba2a547c08ea365
Ryan Flach <ryanflach@users.noreply.github.com>
2016-09-20 10:02:42 -0600

    Add functionality for user to search by zip code - Finish refactor into service and model - Currently an imperfect (but working) solution for diplaying the total results in the view

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,10 +1,5 @@
 class StoresController < ApplicationController
   def index
     @stores = Store.closest_15_stores_within_25_miles(params[:zip_code])
-    response = BestBuyService.new
-                             .closest_15_stores_within_25_miles(
-                               params[:zip_code]
-                             )
-    @total = response[:total]
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,15 +1,19 @@
 class Store
+  attr_reader :total
+
   def initialize(params)
     @params = params
+    @total = @@total
   end
 
-  def service
+  def self.service
     @@service = BestBuyService.new
   end
 
-  def closest_15_stores_within_25_miles(zip_code)
-    stores = service.closest_15_stores_within_25_miles(zip_code)
-    stores.map { |store| Store.new(store[:stores])}
+  def self.closest_15_stores_within_25_miles(zip_code)
+    raw_stores = service.closest_15_stores_within_25_miles(zip_code)
+    @@total = raw_stores[:total]
+    raw_stores[:stores].map { |store| Store.new(store) }
   end
 
   def name
