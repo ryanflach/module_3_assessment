eff4f9d36d97a8f0c9a6609c675e3f83728a3ed6
Ryan Flach <ryanflach@users.noreply.github.com>
2016-09-20 11:41:49 -0600

    Add API endpoint for deleting an individual item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   respond_to :json
-  
+
   def index
     @items = Item.all
   end
--- spec/requests/items/request_delete_item_spec.rb
+++ spec/requests/items/request_delete_item_spec.rb
@@ -1,24 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Request delete item' do
-  context 'deletes successfully' do
-    it 'deletes an item' do
-      item = create(:item)
+  it 'deletes an item' do
+    item = create(:item)
 
-      expect(Item.count).to eq(1)
+    expect(Item.count).to eq(1)
 
-      delete '/api/v1/items/1'
+    delete '/api/v1/items/1'
 
-      expect(response).to have_http_status(204)
-      expect(Item.count).to eq(0)
-    end
-  end
-
-  context 'unable to delete' do
-    it 'attempts to delete an item' do
-      delete '/api/v1/items/1'
-
-      expect(response).to have_http_status(404)
-    end
+    expect(response).to have_http_status(204)
+    expect(Item.count).to eq(0)
   end
 end
